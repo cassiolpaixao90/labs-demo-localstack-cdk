3a2ef39c41168e70d07653b3b3d9b605ba287cca
cassiolpaixao90 <cassiopaixaoti@gmail.com>
2024-07-28 16:30:28 -0300

    Initial commit

---
+++ test/demo-localstack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DemoLocalstack from '../lib/demo-localstack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/demo-localstack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DemoLocalstack.DemoLocalstackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
