a7f5db9d3bf9925c24de98565f68c8a84b2dc9dc
cassiolpaixao90 <cassiopaixaoti@gmail.com>
2024-07-29 07:19:47 -0300

    chore: rename folder

--- infra/index.ts
+++ infra/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import {DemoLocalstackStack} from '../lib/demo-localstack-stack';
+import { DemoLocalstackStack } from './core'
 
 const app = new cdk.App();
 new DemoLocalstackStack(app, 'DemoLocalstackStack', {});
